c072a7c51471130cdf17d639fdb91683bc762afa
Jovan <jovandias9@gmail.com>
2022-07-14 15:11:51 -0400

    fixed the error by adding ; to the printstatement

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         Scanner scan = new Scanner(System.in);
-        String message = nextline();
+        
+        System.out.println("What do you want me to say?");
+        String message = scan.nextLine();
+        System.out.println(message);
     }
 }
