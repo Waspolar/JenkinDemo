1a129c466c8811cf4d5797cce5ce76e0ed5fff7f
Jovan <jovandias9@gmail.com>
2022-07-14 15:09:30 -0400

    added message variable and scanner variable, along with printing the input of the user

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         Scanner scan = new Scanner(System.in);
+        String message = nextline();
     }
 }
