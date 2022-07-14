4f3af32c4cf8a699d1a6528330d363041d0c7238
Jovan <jovandias9@gmail.com>
2022-07-14 13:25:02 -0400

    added scanner and made variable for it

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        Scanner scan = new Scanner(System.in);
     }
 }
